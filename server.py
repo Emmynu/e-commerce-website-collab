ee5049b8f7a8fdc10de065a8512703896802c610
techninja8 <121986463+techninja8@users.noreply.github.com>
2023-05-07 10:46:41 +0100

    Update server.py
    don't regard

--- server.py
+++ server.py
@@ -1,1 +1,39 @@
+import numpy 
+import django 
+import techninja_server
+#import """What Else"""
+
+"""This is is a protoype for backend"""
+#response code from server response
+#will update later 
+server_response = {
+success_msg = "SUCCESS"
+
+error_msg_404 = "NOT_FOUND"
+
+error_msg_400 = "BAD_REQUEST"
+
+server_active = True 
+
+success_msg = "SUCCESS"
+
+error_msg_404 = "NOT_FOUND"
+
+
+
+
+}
+
+#useless... would put in techninja_server
+
+def http_request(return_code, server_active_resp)
+
+def http_response(processed_resp, server_active_resp)
+
+while server_active:
+  #process ...
+  a = 
+  if 
+
+
 
